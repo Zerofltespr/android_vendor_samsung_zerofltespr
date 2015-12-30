5d0d080d19bd3a1488ca4b5119d9626b94a67a0e
W.D <djvoleur@gmail.com>
2015-12-30 01:16:15 -0700

    Add librmnetctl

--- nobleltespr-vendor-blobs.mk
+++ nobleltespr-vendor-blobs.mk
@@ -228,6 +228,8 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/nobleltespr/proprietary/lib64/libsecril-client.so:system/lib64/libsecril-client.so \
     vendor/samsung/nobleltespr/proprietary/lib/libmdmdetect.so:system/lib/libmdmdetect.so \
     vendor/samsung/nobleltespr/proprietary/lib64/libmdmdetect.so:system/lib64/libmdmdetect.so \
+    vendor/samsung/nobleltespr/proprietary/lib/librmnetctl.so:system/lib/librmnetctl.so \
+    vendor/samsung/nobleltespr/proprietary/lib64/librmnetctl.so:system/lib64/librmnetctl.so \
     vendor/samsung/nobleltespr/proprietary/vendor/lib/libdiag.so:system/vendor/lib/libdiag.so \
     vendor/samsung/nobleltespr/proprietary/vendor/lib64/libdiag.so:system/vendor/lib64/libdiag.so \
     vendor/samsung/nobleltespr/proprietary/vendor/lib/libdsutils.so:system/vendor/lib/libdsutils.so \
