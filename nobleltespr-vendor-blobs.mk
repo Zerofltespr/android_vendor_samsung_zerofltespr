d290db60abaf7cebfc4fd7e131de8e0ea60cde14
W.D <djvoleur@gmail.com>
2016-01-04 04:31:56 -0700

    Add libcsd-client

--- nobleltespr-vendor-blobs.mk
+++ nobleltespr-vendor-blobs.mk
@@ -224,6 +224,8 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/nobleltespr/proprietary/bin/argosd:system/bin/argosd \
     vendor/samsung/nobleltespr/proprietary/lib/libatparser.so:system/lib/libatparser.so \
     vendor/samsung/nobleltespr/proprietary/lib64/libatparser.so:system/lib64/libatparser.so \
+    vendor/samsung/nobleltespr/proprietary/lib/libcsd-client.so:system/lib/libcsd-client.so \
+    vendor/samsung/nobleltespr/proprietary/lib64/libcsd-client.so:system/lib64/libcsd-client.so \
     vendor/samsung/nobleltespr/proprietary/lib/libsecnativefeature.so:system/lib/libsecnativefeature.so \
     vendor/samsung/nobleltespr/proprietary/lib64/libsecnativefeature.so:system/lib64/libsecnativefeature.so \
     vendor/samsung/nobleltespr/proprietary/lib/libril.so:system/lib/libril.so \
